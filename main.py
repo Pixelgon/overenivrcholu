77fe0c54321591b3bdb0c229f718b2fa4130781e
Pixelgon <pixelgon4@gmail.com>
2022-02-11 18:20:12 +0100

    Okomentováno + doladěno

--- main.py
+++ main.py
@@ -1,28 +1,26 @@
 # Jedeme do kopce
 # Matej Matejka
 
-input = open("vrcholy.txt", "r")
+vrcholy = open("vrcholy.txt", "r")
 
-t = int(input.readline())
+t = int(vrcholy.readline())
 
-for x in range(0, t - 1):
-    pocet_cisel = int(input.readline())
-    nadmorske_vysky_in = input.readline().strip()
+# Pro kazdy seznam vrcholu
+for x in range(0, t):
+    pocet_cisel = int(vrcholy.readline())
+    nadmorske_vysky_in = vrcholy.readline().strip()
     nadmorske_vysky = nadmorske_vysky_in.split(" ")
     nadmorske_vysky = list(map(int, nadmorske_vysky))
     lze = True
+
+    # Udelej pro cisla v seznamu od 0. do předposledního
     for i in range(0, pocet_cisel - 1):
         if nadmorske_vysky[i + 1] - nadmorske_vysky[i] > 3:
             lze = False
             break
-    if lze == True:
+
+    if lze is True:
         print("ANO")
     else:
         print("NE")
-
-
-
-
-
-
-
+exit()
